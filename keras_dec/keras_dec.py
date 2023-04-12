b4e0a884b24bce108f26067c1fae07f949710ad2
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2023-04-12 15:11:44 +0100

    Add comment

--- keras_dec/keras_dec.py
+++ keras_dec/keras_dec.py
@@ -277,7 +277,9 @@ class DeepEmbeddingClustering(object):
         return
 
     def cluster_acc(self, y_true, y_pred):
+        #Check y_true and y_pred are the same size
         assert y_pred.size == y_true.size
+        #Generate the cost matrix
         D = max(y_pred.max(), y_true.max())+1
         w = np.zeros((D, D), dtype=np.int64)
         for i in range(y_pred.size):
