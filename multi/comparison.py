6168cbd1524e56f922d281334ace73153302095a
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2023-04-19 14:33:29 +0100

    Add rand index and accuracy score for arrays of cluster labels

--- multi/comparison.py
+++ multi/comparison.py
@@ -2,6 +2,8 @@ import numpy as np
 from sklearn.metrics import adjusted_rand_score
 from itertools import combinations
 
+from keras_dec.keras_dec import DeepEmbeddingClustering
+
 
 def mean_rand_index(df):
     """
@@ -22,3 +24,53 @@ def mean_rand_index(df):
 
     # Return the mean Rand index for all pairs of columns
     return np.mean(rand_indices)
+
+
+def rand_index_arr(df,labels):
+    """
+    Calculate the Rand index for all columns in a pandas DataFrame 'df'
+    (where each column represents a different set of cluster labels), and a
+    set of labels 'labels'
+
+    Parameters
+    ----------
+    df : DataFrame
+        Each column is a set of cluster labels. dtype must be int
+    labels : array
+        cluster labels, of the same length as the number of rows in df.
+        dtype must be int
+
+    Returns
+    -------
+    rand_arr : numpy array
+        Array of rand index corresponding to each column in df
+
+    """
+    
+    rand_arr = [adjusted_rand_score(labels,df[col]) for col in df.columns]
+    return rand_arr
+
+
+def accuracy_arr(df,labels):
+    """
+    Calculate the accuracy for all columns in a pandas DataFrame 'df'
+    (where each column represents a different set of cluster labels), compared 
+    to a set of labels 'labels'
+
+    Parameters
+    ----------
+    df : DataFrame
+        Each column is a set of cluster labels. dtype must be int
+    labels : array
+        cluster labels, of the same length as the number of rows in df.
+        dtype must be int
+
+    Returns
+    -------
+    acc_arr : numpy array
+        Array of accuracy corresponding to each column in df
+
+    """
+    c = DeepEmbeddingClustering(n_clusters=10,input_dim=(784))
+    acc_arr = [c.cluster_acc(labels,df[col])[0] for col in df.columns]
+    return acc_arr
