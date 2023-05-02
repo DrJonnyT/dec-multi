93ce8549204dc848b0c03e91ce80601bc4065717
Jonny Taylor <42604380+DrJonnyT@users.noreply.github.com>
2023-05-02 11:47:54 +0100

    Create full mnist params file

---
+++ params/params_full_mnist.py
@@ -0,0 +1,8 @@
+#Setup what sizes of dataset we would like
+#The maximum is 6313, so base it off that
+#This number is the number of each digit we will use
+n10_array = [0]
+n_runs = 100
+n_clusters = 10
+csv_folder = "./output_full_mnist_100/"
+resample=False
