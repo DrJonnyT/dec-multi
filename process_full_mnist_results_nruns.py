4b95c6d84cffa49c6288d43111f49b32e87ac410
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2023-05-04 17:18:24 +0100

    Create file with basic calcs

---
+++ process_full_mnist_results_nruns.py
@@ -0,0 +1,61 @@
+"""
+Explore the impact of number of runs of DEC using the full MNIST dataset
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+
+#Parameters file
+import params.params_full_mnist as params
+
+from mnist.plot import plot_mnist_10x10
+from multi.comparison import mean_rand_index, rand_index_arr, accuracy_arr
+from keras_dec.functions import align_cluster_labels, cluster_acc, modal_labels
+
+
+
+#Load csv
+csv_path = params.csv_folder + "dec_0.csv"
+labels_path = params.csv_folder + "dec_0_labels.csv"
+
+#6313 of each digit
+# csv_path = "./output_no_resample_100/dec_6313.csv"
+# labels_path = "./output_no_resample_100/dec_6313_labels.csv"
+
+
+
+df_dec = pd.read_csv(csv_path,index_col=0)
+df_labels = pd.read_csv(labels_path,index_col=0)
+#All label columns are the same
+labels = df_labels['labels_1']
+
+#Align results
+df_dec_aligned = pd.DataFrame(index=df_dec.index)
+
+#Ignore a fragmentation performance warning, it was quicker doing it this way
+with warnings.catch_warnings():
+    warnings.simplefilter(action='ignore', category=Warning)
+    for col in df_dec.columns:
+        df_dec_aligned[col] = align_cluster_labels(df_dec.iloc[:,0],df_dec[col])    
+
+
+#Mode accruacy
+dec_mode_labels = modal_labels(df_dec_aligned)
+acc = cluster_acc(labels, dec_mode_labels)[0]
+
+
+
+#Array of accruacy of individual runs
+ds_acc = pd.Series(index=df_dec_aligned.columns)
+ds_prec = pd.Series(index=df_dec_aligned.columns)
+for col in df_dec_aligned.columns:
+    ds_acc[col] = cluster_acc(labels, df_dec_aligned[col])[0]
+    ds_prec[col] = cluster_acc(dec_mode_labels, df_dec_aligned[col])[0]
+    
+
+#For some reason in the full mnist data you are getting way too many 3's in the clusters
+#So that is limiting the accuracy.
+    
+    
