df44d2a93e22535439f33509dc016724ce29978d
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2023-05-05 13:42:29 +0100

    Remove params file and copy file to output when running

--- run_dec_many_times.py
+++ run_dec_many_times.py
@@ -3,28 +3,33 @@ import tensorflow as tf
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
+import shutil
+import os
+
 from multi.dec import dec_mnist_n_times_csv
-import params.params_full_mnist as params
 
 #Run Deep embedded clustering lots of times and save the results to csv files
 #Load settings from params.py file
 #An array of the number of copies of each digit to use
-n10_array = params.n10_array
+n_digits_array = [6130]
 #The number of times to run kmeans
-n_runs = params.n_runs
+n_runs = 2
 #Number of clusters
-n_clusters = params.n_clusters
+n_clusters = 10
 #Output folder
-csv_folder = params.csv_folder
-#Resample flag
-resample=params.resample
+output_folder = "./output_random_sample/"
+
+#Copy this file and params file into the output folder
+os.makedirs(os.path.dirname(output_folder), exist_ok=True)
+shutil.copyfile("./run_dec_many_times.py", output_folder+"run_dec_many_times.py")
+
 
 #Loop through different sized datasets
-for n10 in n10_array:
-        csv_file = csv_folder + f"dec_{n10}.csv"
+for n_digits in n_digits_array:
+        csv_file = output_folder + f"dec_{n_digits}.csv"
         #Full version
-        dec_mnist_n_times_csv(n10, n_runs, n_clusters,csv_file,newcsv=False,
-                    fail_tolerance=10000)
         # dec_mnist_n_times_csv(n10, n_runs, n_clusters,csv_file,newcsv=False,
-        #             iter_max=100,
-        #             finetune_iters=10000,layerwise_pretrain_iters=5000,resample=resample,fail_tolerance=1000)
+        #             fail_tolerance=10000)
+        dec_mnist_n_times_csv(n_digits, n_runs, n_clusters,csv_file,newcsv=False,
+                    iter_max=100,
+                    finetune_iters=1000,layerwise_pretrain_iters=500,resample=False,fail_tolerance=1000)
