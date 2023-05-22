37a3778d2ddeaf1269c59e8db3922e91efec7e30
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2023-05-22 10:33:06 +0100

    Remove params folder as no longer used

--- params/params_full_mnist.py
+++
@@ -1,8 +0,0 @@
-#Setup what sizes of dataset we would like
-#The maximum is 6313, so base it off that
-#This number is the number of each digit we will use
-n10_array = [0]
-n_runs = 200
-n_clusters = 10
-csv_folder = "./output_full_mnist_def_100/"
-resample=False
--- params/params_no_resample.py
+++
@@ -1,9 +0,0 @@
-#Setup what sizes of dataset we would like
-#The maximum is 6313, so base it off that
-#This number is the number of each digit we will use
-#n10_array = [6,63,631,6313]
-n10_array = [631]
-n_runs = 25
-n_clusters = 10
-csv_folder = "./output_no_resample_again/"
-resample=False
